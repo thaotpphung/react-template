32166e6013c56a7c38e6c4535f5a6ca6d8617740
thaotpphung <thaotpphung@users.noreply.github.com>
2024-06-24 21:08:42 +0000

    deploy: c53a6ab9b41d168c21b42aff7474198d4fcda4db

--- assets/not-found-uQr-WxFZ.js
+++
@@ -1,1 +0,0 @@
-import{j as e,w as o}from"./index-Qa_WIuNp.js";const r=()=>e.jsxs("div",{className:"mt-52 flex flex-col items-center font-semibold",children:[e.jsx("h1",{children:"404 - Not Found"}),e.jsx("p",{children:"Sorry, the page you are looking for does not exist."}),e.jsx(o,{to:"/",replace:!0,children:"Go to Home"})]});export{r as NotFoundRoute};
