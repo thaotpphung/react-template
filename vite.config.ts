1628c698d14260d906f6f2c45c5ecdee42128418
Thao Phung <thao.tp.phung@gmail.com>
2024-06-24 18:25:44 -0400

    ci: add config for base url depending on environment

--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,25 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  base: '/kocokeeper/',
-  plugins: [react(), viteTsconfigPaths()],
-  server: {
-    port: 3000,
-  },
-  preview: {
-    port: 3000,
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/testing/setup-tests.ts',
-    exclude: ['**/node_modules/**', '**/e2e/**'],
-    coverage: {
-      include: ['src/**'],
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/kocokeeper/' : './',
+    plugins: [react(), viteTsconfigPaths()],
+    server: {
+      port: 3000,
     },
-  },
-  optimizeDeps: { exclude: ['fsevents'] },
+    preview: {
+      port: 3000,
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/testing/setup-tests.ts',
+      exclude: ['**/node_modules/**', '**/e2e/**'],
+      coverage: {
+        include: ['src/**'],
+      },
+    },
+    optimizeDeps: { exclude: ['fsevents'] },
+  };
 });
