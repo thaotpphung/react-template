e6856f9581131ed58e10a8b956f8ea3896c62303
thaotpphung <thaotpphung@users.noreply.github.com>
2024-07-06 17:19:48 +0000

    deploy: 1628c698d14260d906f6f2c45c5ecdee42128418

---
+++ assets/not-found-uAdYDkug.js
@@ -0,0 +1,1 @@
+import{j as e,w as o}from"./index-Btb8dkF1.js";const r=()=>e.jsxs("div",{className:"mt-52 flex flex-col items-center font-semibold",children:[e.jsx("h1",{children:"404 - Not Found"}),e.jsx("p",{children:"Sorry, the page you are looking for does not exist."}),e.jsx(o,{to:"/",replace:!0,children:"Go to Home"})]});export{r as NotFoundRoute};
